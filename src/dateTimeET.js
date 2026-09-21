47b70b11cc2f212d8fa91e80e66f3b5ecb48522a
Marie Abel <abelm2007@users.noreply.github.com>
2026-09-21 11:43:12 +0300

    Esimene node veeb loodud

---
+++ src/dateTimeET.js
@@ -0,0 +1,38 @@
+const dateFormattedET = function(calendarType){
+	let timeNow = new Date();
+	const monthNamesET = ['jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember'];
+	const folkMonthNamesET = ['näärikuu', 'radokuu', 'urbekuu', 'jürikuu', 'lehekuu', 'jaanikuu', 'heinakuu', 'põimukuu', 'mihklikuu', 'viinakuu', 'talvekuu', 'jõulukuu'];
+	if(calendarType == 1){
+		return timeNow.getDate() + '. ' + monthNamesET[timeNow.getMonth()] + ' ' + timeNow.getFullYear();
+	}
+	if(calendarType == 0){
+		return timeNow.getDate() + '. ' + folkMonthNamesET[timeNow.getMonth()] + ' ' + timeNow.getFullYear();
+	}
+}
+	
+const timeFormattedET = function(){
+	let timeNow = new Date();
+	let hourNow = timeNow.getHours();
+	let minuteNow = timeNow.getMinutes();
+	let secondNow = timeNow.getSeconds();
+	if(minuteNow < 10){
+		minuteNow = '0' + minuteNow
+	}
+	if(secondNow < 10){
+		secondNow = '0' + secondNow
+	}
+	let timeFormatted = hourNow + ":" + minuteNow + ':' + secondNow;
+	return timeFormatted;
+}
+
+const weekdayET = function(){ 
+	let weekDay = new Date().getDay();
+	const weekdayNamesET = ['pühapäev ', 'esmaspäev ', 'teisipäev ', 'kolmapäev ', 'neljapäev ', 'reede ', 'laupäev '];
+	return weekdayNamesET[weekDay];
+}
+//ekspordin kõik vajalikud
+module.exports = {
+	fullDate: dateFormattedET,
+	fullTime: timeFormattedET,
+	weekday: weekdayET
+}
---
+++ www_3.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const pageHead = '<!DOCTYPE html>\n<html lang="et">\n<head>\n\t<meta charset="utf-8">\n\t<title>Marie Abel, veevbiprogrammeerimine</title>\n</head>\n<body>\n';
+const pageBody = '\t<h1>Marie Abel, veebiprogrammeerimine</h1>\n\t <p>See leht on loodud veebiprogrammeerimise kursusel <a href="https://www.tlu.ee">Tallinna Ülikoolis</a> ning ei sislda tõsiseltvõetavat sisu!</p>\n\t<p>Esialgu tutvusime lihtsalt HTML keelega, peatselt programmeerime.</p>\n\t<hr>';
+const pageFoot = '\n</body>\n</html>';
+const dateTimeET = require('./src/dateTimeET.js'); 
+
+http.createServer(function(req, res){
+	res.writeHead(200, {"Content-type": "text/html"});
+	res.write(pageHead);
+	res.write(pageBody);
+	res.write('<p>Tänane kuupäev: ' + dateTimeET.weekday() + ' ' + dateTimeET.fullDate(0) + '</p>');
+    res.write('<p>Praegu on kell: ' + dateTimeET.fullTime() + '</p>');
+	res.write(pageFoot);
+	res.write('Veeb läkski käima!');
+	return res.end();
+}).listen(5119);
